a44bb173de9e016fd4d43cdff20cde39abaa3972
Junho <poiewq1731@gmail.com>
2018-12-18 11:16:17 +0900

    spit

---
+++ WebContent/controller/app.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+	'jquery.sap.global',
+	'sap/ui/core/mvc/Controller',
+	'sap/m/Popover',
+	'sap/m/Button',
+	'sap/ui/model/odata/v2/ODataModel'
+], function(jQuery, Controller, Popover, Button, ODataModel) {
+	"use strict";
+
+	return Controller.extend("uifiveApp.controller.app", {
+
+		onInit: function () {
+			
+		},
+
+		onMenuOpen: function (event) {
+			var oSplitApp = this.getView().byId("mainApp"),
+				sAppMode = oSplitApp.getMode();
+			
+			if(sAppMode === "HideMode"){
+				oSplitApp.setMode("ShowHideMode");
+			}else{
+				oSplitApp.setMode("HideMode");
+			}
+		},
+		onHideContent : function(oEvent){
+			var obj = oEvent.getSource();
+		}
+	});
+
+});
